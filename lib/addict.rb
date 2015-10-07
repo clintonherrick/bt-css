319e77e10c47ef6918fe4fc2bb20b21bcff6577b
Trevor Scherling & Brandon Seroyer <student@epicodus.com>
2015-10-07 16:09:02 -0700

    added and passed name validation spec for Addict class

--- lib/addict.rb
+++ lib/addict.rb
@@ -1,3 +1,4 @@
 class Addict < ActiveRecord::Base
   has_many(:relapses)
+  validates(:name, {:presence => true })
 end
--- spec/addict_spec.rb
+++ spec/addict_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Addict do
-    it { should have_many(:relapses) }
+  it { should have_many(:relapses) }
+
+  it("validates presence of name") do
+    addict = Addict.new({:name => ''})
+    expect(addict.save()).to(eq(false))
+
+  end
 end
