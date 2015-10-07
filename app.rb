152f4d99ffefee97d56b0fd7016a0bbba18f1ce2
Trevor Scherling & Brandon Seroyer <student@epicodus.com>
2015-10-07 16:56:30 -0700

    addict profile erb added and routed

--- app.rb
+++ app.rb
@@ -15,3 +15,25 @@ get('/addicts') do
   @addicts = Addict.all()
   erb(:addicts)
 end
+
+post('/addicts') do
+  name = params.fetch('name')
+  @addict = Addict.new({name: name})
+  @addict.save()
+  @addicts = Addict.all()
+  erb(:addicts)
+end
+
+get ('/addicts/:id') do
+  @addict = Addict.find(params.fetch('id').to_i())
+  erb(:addict_profile)
+end
+
+post('/add_relapse') do
+  new_relapse = params.fetch('relapse')
+  addict_relapse = Relapse.new({:relapse => new_relapse})
+  id = params.fetch('addict_id')
+  @addict = Addict.find(id.to_i())
+  @addict.add_relapse(addict_relapse)
+  redirect('/addicts/' + @addict.id().to_s())
+end
