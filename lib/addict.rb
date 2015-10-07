a7bb7022cc607220717fb8a9382fe1a0831e38b2
Trevor Scherling & Brandon Seroyer <student@epicodus.com>
2015-10-07 16:23:12 -0700

    addict class specs complete

--- lib/addict.rb
+++ lib/addict.rb
@@ -1,4 +1,11 @@
 class Addict < ActiveRecord::Base
   has_many(:relapses)
-  validates(:name, {:presence => true })
+  validates(:name, {:presence => true, :length => { :minimum => 1 }})
+  before_save(:capitalize_name)
+
+  private
+
+  def capitalize_name 
+    self.name=(name().split.map {|i| i.capitalize }.join(' '))
+  end
 end
--- spec/addict_spec.rb
+++ spec/addict_spec.rb
@@ -6,6 +6,15 @@ describe Addict do
   it("validates presence of name") do
     addict = Addict.new({:name => ''})
     expect(addict.save()).to(eq(false))
+  end
 
+  it("ensures the length of name is at least 1 character long") do
+    addict = Addict.new({:name => ''.*(1)})
+    expect(addict.save()).to(eq(false))
+  end
+
+  it("capitalize the name") do
+    addict = Addict.create({:name => "trevor"})
+    expect(addict.name()).to(eq("Trevor"))
   end
 end
